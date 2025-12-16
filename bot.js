261a83973557dc13ae3d41fd80052d763b519243
Сергей <germes63@bk.ru>
2025-12-16 21:02:47 +0300

    fix: add port binding for render

--- bot.js
+++ bot.js
@@ -20,3 +20,9 @@ bot.command("start", async (ctx) => {
 // Запуск бота
 bot.start();
 console.log("Бот запущен...");
+
+const http = require("http");
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Bot is running!");
+}).listen(process.env.PORT || 8080);
